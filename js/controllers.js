930ed178d3a8736023f9f03d2032d5064199971c
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-11-04 12:34:08 -0500

    adds JSON data and table to html

--- js/controllers.js
+++ js/controllers.js
@@ -1,31 +1,161 @@
-//
-// app.controller('HomeController', ['$scope', '$http', '$routeParams', '$location', 'omdbSearch', function($scope, $http, $routeParams, $location, omdbSearch){
-//     $scope.booyah ='booyah';
-//
-//     $scope.searchOmdb = function(search){
-//         //line below is in order to save this.query in the service object in the services.js document
-//         omdbSearch.search(search);
-//         $location.path('/results')
-//     };
-// }]
-// })
-//
-//
-// app.controller('ResultsController', ['$scope', '$http', '$routeParams', '$location', 'omdbSearch', function($scope, $http, $routeParams,$location, omdbSearch){
-//     omdbSearch.results().then(function(stuff){
-//         console.log('search results coming from the second controller', stuff.data.Search);
-//         var movies = stuff.data.Search;
-//     })
-// }])
-
-
 app.controller('HomeController', ['$scope',
 '$http',
 '$location',
-// 'cartPersistence',
+// 'caffeineMeter',
 function($scope, $http, $location
-    // , cartPersistence
+    // , caffeineMeter
 ){
 
     $scope.thisbooyah = 'wiring is working!'
-}])
+
+    $scope.teas = {};
+    $scope.teas.teas = [
+    {
+        "_id": "55c8ee82152165d244b98300",
+        "name": "Bayard stew",
+        "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
+        "caffeineScale": 244,
+        "price": 1540,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
+        "__v": 0,
+        "categories": [ "dark", "cold"]
+    },
+
+    {
+        "_id": "55c8ee82152165d244b98301",
+        "name": "Incompactness syrup",
+        "ingredients": "fennel, nutmeg leaves, parsley, cream of 'cream of cream', blarney",
+        "caffeineScale": 49,
+        "price": 7348,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+        "__v": 0,
+        "categories": ["awesome"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98302",
+        "name": "Flexner white tea",
+        "ingredients": "hot sauce, iron, beetle nut, fresco, blarney, raw mashed potato",
+        "caffeineScale": 38,
+        "price": 4991,
+        "inStock": true,
+        "rating": 4,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "__v": 0,
+        "categories": ["cold"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98303",
+        "name": "Pressor leaf",
+        "ingredients": "purina chow, flavorings, pepper, acorns, quality tallow, old sock, bay leaf",
+        "caffeineScale": 153,
+        "price": 5496,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "__v": 0,
+        "categories": ["dry", "hot", "awesome"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98304",
+        "name": "Flexner veggie tea",
+        "ingredients": "cream of tartar, eggplant, cake, deer antler",
+        "caffeineScale": 181,
+        "price": 2445,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+        "__v": 0,
+        "categories": ["summer"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98305",
+        "name": "Topflighter malt",
+        "ingredients": "botox, toast, cream of 'cream of 'cream of cream'', kitchen scraps, beef, aligator tongue, lawn clippings",
+        "caffeineScale": 241,
+        "price": 4486,
+        "inStock": true,
+        "rating": 3,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31359_d?$cimg$",
+        "__v": 0,
+        "categories": ["dry","lucid","warm"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98306",
+        "name": "Cooking mix",
+        "ingredients": "flavorings, roasted mushrooms, toast, tumeric",
+        "caffeineScale": 230,
+        "price": 6973,
+        "inStock": true,
+        "rating": 3,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+        "__v": 0,
+        "categories": ["summer"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98307",
+        "name": "Cooking stew",
+        "ingredients": "eggplant",
+        "caffeineScale": 122,
+        "price": 6003,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "__v": 0,
+        "categories": ["dry","winter","lucid"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98308",
+        "name": "Prevenient herb tea",
+        "ingredients": "cream of tartar, cream of cream, kitchen scraps, flavorings",
+        "caffeineScale": 196,
+        "price": 1374,
+        "inStock": true,
+        "rating": 3,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32174_d?$cimg$",
+        "__v": 0,
+        "categories": ["lucid","hot"]
+    },
+    {
+        "_id": "55c8ee82152165d244b98309",
+        "name": "Angular mix",
+        "ingredients": "hot sauce, lawn clippings, fennel, parsley, quinine",
+        "caffeineScale": 196,
+        "price": 4158,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+        "__v": 0,
+        "categories": ["spring", "warm","winter"]
+    }
+]
+
+
+
+    $scope.nextThing = ''
+}]);
+
+
+
+
+
+
+
+
+
+app.directive('caffeineMeter', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            caffeinemg: '=',
+        },
+        templateUrl: '/partials/caffeine-meter-directive.html',
+        controller: function($scope){
+            alert($scope.teas);
+
+        }
+    }
+})
---
+++ js/directives.js
@@ -0,0 +1,13 @@
+// app.directive('caffeineMeter', function(){
+//     return {
+//         restrict: 'E',
+//         scope: {
+//             caffeinemg: '=',
+//         },
+//         templateUrl: '/partials/caffeine-meter-directive.html',
+//         controller: function($scope){
+//             alert($scope.teas);
+//
+//         }
+//     }
+// })
