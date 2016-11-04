2e7873c1701265161471f8a6b64b50d14da70302
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-11-04 15:47:54 -0500

    it block skeletons

--- spec/spec.js
+++ spec/spec.js
@@ -1,38 +1,99 @@
 describe('angular tea shopping cart app', function() {
+        it('should display image, caffeine scale, ingredients, rating', function() {
+            browser.get('http://localhost:8000');
+
+        });
 
 
-    beforeEach(function(){
-        browser.get('http://localhost:8000');
-    })
+        it('it should display price in currency', function() {
+            browser.get('http://localhost:8000');
+
+        });
 
 
-  it('should ', function() {
+        it('it should display quantity dropdown', function() {
+            browser.get('http://localhost:8000');
 
-    });
-
-
-it should display image, price, caffeine scale, ingredients, rating,
-it should have in stock displayed as yes or no
-it should display price price in currency
-it should display quantity dropdown
-it should add at least one tea to bag if addToBag button clicked
-it should add correct quantity of product to bag when dropdown displays text and button clicked
-it should preserve quantity of teas in bag on second page
-it should have checkout buttons value display quantity increase when button clicked
-it should multiply the quantity by the price and display as subtotal
-it should display the whole total
-it should display teas from the dropdown
-it should display teas from the search bar
-it should sort teas by price, both lowest and highest
+        });
 
 
-//on cart page
-it should have a functional edit quantity button
-it should change the button text to say save when clicked
-it should update the quantity, subtotal price, and total price when save is clicked
-it should have a button that removes the tea from the bag upon click
+        it('it should have whether tea in stock displayed as yes or no', function() {
+            browser.get('http://localhost:8000');
+
+        });
+
+
+        it('it should add at least one tea to cart if addToCart button clicked', function() {
+            browser.get('http://localhost:8000');
+
+            });
+
+
+        it('it should add correct quantity of product to cart when dropdown displays text and button clicked', function() {
+            browser.get('http://localhost:8000');
+
+            });
 
 
 
+        it('it should have checkout buttons value display quantity increase when button clicked', function() {
+            browser.get('http://localhost:8000');
+
+            });
+
+
+        it('it should filter teas from chosen dropdown option', function() {
+            browser.get('http://localhost:8000');
+
+            });
+
+        it('it should display teas from the search bar', function() {
+            browser.get('http://localhost:8000');
+
+        });
+
+        it('it should sort teas by price, both lowest and highest', function() {
+            browser.get('http://localhost:8000');
+
+            });
+
+//-----------------on cart page-----------------
+//-----------------on cart page-----------------
+//-----------------on cart page-----------------
+
+        it('it should preserve quantity of teas in cart on second page', function() {
+            browser.get('http://localhost:8000/cart');
+
+        });
+
+
+        it('it should multiply the quantity by the price and display as subtotal', function() {
+
+        });
+
+
+        it('it should have an edit quantity button', function() {
+
+        });
+
+
+        it('it should change the edit button text to say save when clicked', function() {
+
+        });
+
+
+        it('it should update the quantity, subtotal price, and total price when save is clicked', function() {
+
+        });
+
+
+        it('it should have a button that removes the tea from the cart upon click', function() {
+
+        });
+
+
+        it('it should update quantity, subtotal price, and total price when item deleted from cart', function() {
+
+        });
 
 });
