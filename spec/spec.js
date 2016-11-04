0385766c9d6052efa15e2704a64c325758cb524f
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-11-04 11:04:47 -0500

    sets up initial structure of app

---
+++ spec/spec.js
@@ -0,0 +1,19 @@
+describe('angular tea shopping cart app', function() {
+
+
+    beforeEach(function(){
+        browser.get('http://localhost:8000');
+    })
+    
+
+  it('should ', function() {
+
+    });
+
+
+
+
+
+
+
+});
